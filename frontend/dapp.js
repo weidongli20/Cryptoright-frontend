f859e1b6c9e91243d2e6e280695613fdabb23411
W L <weidong_ca00@yahoo.com>
2020-11-10 15:55:27 -0600

    update files

--- frontend/dapp.js
+++ frontend/dapp.js
@@ -1,5 +1,5 @@
 // Change this address to match your deployed contract!
-const contract_address = "0xa5b70b3f9e02b2d0b85E10042a76b1A7F397Cc6b";
+const contract_address = "0x43DF34038E4fEe36834A1FbfB2ad61Bf72225bad";
 
 const dApp = {
   ethEnabled: function() {
